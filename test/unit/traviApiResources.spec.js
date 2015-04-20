d495f301ac8a6c704779b8a2681f2b7956c2a660
Matt Travi <programmer@travi.org>
2015-04-19 21:52:11 -0500

    built out the service request layer for the api catalog

---
+++ test/unit/traviApiResources.spec.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var traviApiResources = require('../../lib/traviApiResources.js'),
+    traverson = require('traverson'),
+    any = require('../helpers/any');
+
+require('setup-referee-sinon/globals');
+
+suite('travi-api resource interactions', function () {
+    var stubForGet = sinon.stub();
+
+    setup(function () {
+        sinon.stub(traverson, 'from').withArgs('http://api.travi.org/').returns({
+            get: stubForGet
+        });
+    });
+
+    test('that links are requested from the api catalog', function () {
+        var callback = sinon.spy(),
+            links = {
+                'self': any.url(),
+                'foo': any.url()
+            };
+        stubForGet.yields(null, { body: JSON.stringify({ '_links': links })});
+
+        traviApiResources.getLinksFor('catalog', callback);
+
+        assert.calledWith(callback, null, links);
+    });
+});
