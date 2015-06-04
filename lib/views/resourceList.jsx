9b14b02e667ecc78bc4c1225be7d585e3bc1f8a9
Matt Travi <programmer@travi.org>
2015-06-03 20:35:27 -0500

    actually list the resources if the list is not empty

--- lib/views/resourceList.jsx
+++ lib/views/resourceList.jsx
@@ -6,9 +6,20 @@ module.exports = React.createClass({
     render: function () {
         'use strict';
 
+        var resources;
+        if (this.props.resources.length) {
+            resources = <ul>
+                {this.props.resources.map(function (resource) {
+                    return <li>{resource}</li>;
+                })}
+            </ul>;
+        } else {
+            resources = <p className="alert alert-info">No { this.props.resourceType } are available</p>;
+        }
+
         return (
             <Layout types={this.props.types}>
-                <p className="alert alert-info">No { this.props.resourceType } are available</p>
+            {resources}
             </Layout>
         );
     }
--- test/unit/view/pages/resourcesTest.jsx
+++ test/unit/view/pages/resourcesTest.jsx
@@ -3,6 +3,7 @@
 var React = require('react'),
     ReactTestUtils = require('react/lib/ReactTestUtils'),
     assert = require('chai').assert,
+    _ = require('lodash'),
     any = require('../../../helpers/any-for-admin'),
     proxyquire = require('proxyquire'),
     LayoutStub = require('../../../helpers/layoutStub.jsx');
@@ -26,5 +27,33 @@ suite('resource list', function () {
         var content = layoutComponent.props.children;
         assert.equal(content.type, 'p');
         assert.equal(content.props.children.join(''), 'No ' + resourceType + ' are available');
+    });
+
+    test('that resources are listed', function () {
+        var resourceType = any.string(),
+            resources = [
+                'one',
+                'two',
+                'three'
+            ],
+            element = React.createElement(ResourceList, {
+                resources: resources,
+                resourceType: resourceType,
+                types: [
+                    'foo',
+                    'bar'
+                ]
+            }),
+            rendered = ReactTestUtils.renderIntoDocument(element);
+
+        var layoutComponent = ReactTestUtils.findRenderedComponentWithType(rendered, LayoutStub),
+            content = layoutComponent.props.children,
+            items = ReactTestUtils.scryRenderedDOMComponentsWithTag(layoutComponent, 'li');
+
+        assert.equal(content.type, 'ul');
+        assert.equal(items.length, resources.length);
+        _.each(items, function (item, index) {
+            assert.equal(item.getDOMNode().textContent, resources[index]);
+        });
     });
 });
