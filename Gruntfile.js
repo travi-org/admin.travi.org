6ca4abca549fe192565435bbe1d7106cc45a6007
Matt Travi <programmer@travi.org>
2015-06-06 23:47:30 -0500

    added perfbudget task for #11

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,5 +3,9 @@ module.exports = function (grunt) {
     'use strict';
 
     require('time-grunt')(grunt);
-    require('load-grunt-config')(grunt);
+    require('load-grunt-config')(grunt, {
+        config: {
+            webPageTestApiToken: process.env.WEB_PAGE_TEST_API_TOKEN
+        }
+    });
 };
