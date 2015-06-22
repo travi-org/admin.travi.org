2d949d6592a45fb4234fa541703ef6fcad184087
Matt Travi <programmer@travi.org>
2015-06-21 20:17:31 -0500

    refactored build process for listing resources

--- lib/views/resourceList.jsx
+++ lib/views/resourceList.jsx
@@ -8,23 +8,23 @@ module.exports = React.createClass({
 
         var resources;
 
+        function renderResourceAsListItem(resource) {
+            var thumbnail;
+
+            if (resource.thumbnail) {
+                thumbnail = <img src={resource.thumbnail} className="thumbnail" />;
+            } else {
+                thumbnail = '';
+            }
+
+            return <li key={resource.id} className="list-group-item">
+                {thumbnail}
+                {resource.displayName}
+            </li>;
+        }
+
         if (this.props.resources.length) {
-            resources = <ul className="list-group">
-                {this.props.resources.map(function (resource) {
-                    var thumbnail;
-
-                    if (resource.thumbnail) {
-                        thumbnail = <img src={resource.thumbnail} className="thumbnail" />;
-                    } else {
-                        thumbnail = '';
-                    }
-
-                    return <li key={resource.id} className="list-group-item">
-                        {thumbnail}
-                        {resource.displayName}
-                    </li>;
-                })}
-            </ul>;
+            resources = <ul className="list-group"> {this.props.resources.map(renderResourceAsListItem)} </ul>;
         } else {
             resources = <p className="alert alert-info">No { this.props.resourceType } are available</p>;
         }
