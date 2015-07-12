8bbafce4a5a3b1d2e9d9ff3653d1d88af3bc79fa
Matt Travi <programmer@travi.org>
2015-07-12 16:17:22 -0500

    start of grunt task to publish consumer-driven pact for #20

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,4 +8,43 @@ module.exports = function (grunt) {
             webPageTestApiToken: process.env.WEB_PAGE_TEST_API_TOKEN
         }
     });
+
+    grunt.registerTask('pact-consumer', 'consumer driven contract', function () {
+        var extendGruntPlugin = require('extend-grunt-plugin');
+
+        var options = this.options({
+            pactServicePort: 1234,
+            pactDir: 'tmp'
+        });
+
+        extendGruntPlugin(grunt, require('grunt-shell-spawn/tasks/shell'), {
+            'shell.pactServerStart': {
+                command: 'bundle exec pact-mock-service start -p ' +
+                options.pactServicePort + ' -l ' +
+                options.pactDir + '/pact.log --pact-dir ' +
+                options.pactDir + '/pacts',
+                options: {
+                    stdout: true,
+                    stderr: true,
+                    failOnError: true,
+                    async: true
+                }
+            },
+            'shell.pactServerStop': {
+                command: 'bundle exec pact-mock-service stop -p ' + options.pactServicePort,
+                options: {
+                    stdout: true,
+                    stderr: true,
+                    failOnError: true,
+                    async: true
+                }
+            }
+        });
+
+        grunt.task.run('shell:pactServerStart');
+
+        //run tests
+
+        grunt.task.run('shell:pactServerStop');
+    });
 };
