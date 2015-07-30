0a770365d41570e8f32d5a84c0f5a89cb53817ec
Matt Travi <programmer@travi.org>
2015-07-29 23:02:36 -0500

    controller method for getting a resource by id

--- test/unit/resourcesControllerTest.js
+++ test/unit/resourcesControllerTest.js
@@ -11,11 +11,13 @@ require('setup-referee-sinon/globals');
 suite('resources controller', function () {
     setup(function () {
         sinon.stub(traviApiResources, 'getListOf');
+        sinon.stub(traviApiResources, 'getResourceBy');
         sinon.stub(resourceMapperFactory, 'getMapperFor');
     });
 
     teardown(function () {
         traviApiResources.getListOf.restore();
+        traviApiResources.getResourceBy.restore();
         resourceMapperFactory.getMapperFor.restore();
     });
 
@@ -35,5 +37,21 @@ suite('resources controller', function () {
         resourcesController.getListOf(resourceType, callback);
 
         assert.calledWith(callback, null, mappedList);
+    });
+
+    test('that resource is requested from the api', function () {
+        var resourceType = any.string(),
+            resourceId = any.string(),
+            callback = sinon.spy(),
+            resource = any.resource(),
+            mappedResource = {foo: 'bar'};
+        traviApiResources.getResourceBy.withArgs(resourceType, resourceId).yields(null, resource);
+        resourceMapperFactory.getMapperFor.withArgs(resourceType).returns({
+            mapToView: sinon.stub().withArgs(resource).returns(mappedResource)
+        });
+
+        resourcesController.getResource(resourceType, resourceId, callback);
+
+        assert.calledWith(callback, null, mappedResource);
     });
 });
