6e3033093d34c013ecb6b359f0db8821da5e9a44
Matt Travi <programmer@travi.org>
2015-10-31 15:34:28 -0500

    added a view for not-found

---
+++ lib/views/not-found.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+module.exports = React.createClass({
+    render: function () {
+        return (
+            <div className="jumbotron">
+                <h2>404</h2>
+                <p>Page Not Found</p>
+            </div>
+        );
+    }
+});
---
+++ test/unit/view/pages/notFoundTest.jsx
@@ -0,0 +1,16 @@
+const
+    React = require('react'),
+    cheerio = require('cheerio'),
+    reactDom = require('react-dom/server'),
+
+    NotFound = require('../../../../lib/views/not-found.jsx');
+require('setup-referee-sinon/globals');
+
+suite('not found', function () {
+    test('that the proper content is displayed', function () {
+        const $ = cheerio.load(reactDom.renderToStaticMarkup(<NotFound />));
+
+        assert.equals($('h2').text(), '404');
+        assert.equals($('p').text(), 'Page Not Found');
+    });
+});
