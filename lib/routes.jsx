afd9a4f990ecdce5a657ccfe251fde5b86eab10c
Matt Travi <programmer@travi.org>
2015-11-01 20:50:50 -0600

    added routes to the router

--- lib/routes.jsx
+++ lib/routes.jsx
@@ -1,12 +1,22 @@
 'use strict';
 
 const
-    React = require('react'), //eslint-disable-line no-unused-vars
+    React = require('react'),
     router = require('react-router'),
     Route = router.Route,
+    IndexRoute = router.IndexRoute,
 
-    Index = require('./views/index.jsx');
+    Wrap = require('./views/theme/wrap.jsx'),
+    Index = require('./views/index.jsx')(React),
+    ResourceList = require('./views/resourceList.jsx'),
+
+    NotFound = require('./views/not-found.jsx')(React);
 
 module.exports = (
-    <Route path='/' component={Index} />
+    <Route path='/' component={Wrap}>
+        <IndexRoute component={Index} />
+        <Route path='rides' component={ResourceList} />
+        <Route path='users' component={ResourceList} />
+        <Route path="*" component={NotFound}/>
+    </Route>
 );
--- test/unit/routesTest.jsx
+++ test/unit/routesTest.jsx
@@ -1,15 +1,31 @@
 const
     React = require('react'), //eslint-disable-line no-unused-vars
+    ReactTestUtils = require('react-addons-test-utils'),
+    shallowRenderer = ReactTestUtils.createRenderer(),
     dom = require('react-dom'),
     reactRouter = require('react-router'),
     Router = reactRouter.Router,
+    createHistory = require('history/lib/createMemoryHistory'),
 
-    routes = require('../../lib/routes.jsx');
+    proxyquire = require('proxyquire'),
+    routes = proxyquire('../../lib/routes.jsx', {
+        './views/theme/wrap.jsx': React.createClass({
+            render: function () {
+                return (<div>wrapper { this.props.children }</div>);
+            }
+        }),
+        './views/index.jsx': React => () => <div>index</div>,
+        './views/not-found.jsx': React => () => <div>not-found</div>,
+        './views/resourceList.jsx': React.createClass({
+            render: () => <div>resources</div>
+        })
+    });
 
 suite('routes', function () {
     'use strict';
 
     let node;
+
     beforeEach(function () {
         node = document.createElement('div')
     });
@@ -20,11 +36,41 @@ suite('routes', function () {
 
     test('that the root route is defined', function () {
         dom.render((
-            <Router>
+            <Router history={createHistory('/')}>
                 { routes }
             </Router>
         ), node, function () {
-            assert.equals(node.querySelectorAll('#wrap').length, 1);
+            assert.equals(node.textContent, 'wrapper index');
+        });
+    });
+
+    test('that the not-found route is defined', function () {
+        dom.render((
+            <Router history={createHistory('/foo')}>
+                { routes }
+            </Router>
+        ), node, function () {
+            assert.equals(node.textContent, 'wrapper not-found');
+        });
+    });
+
+    test('that the rides route is defined', function () {
+        dom.render((
+            <Router history={createHistory('/rides')}>
+                { routes }
+            </Router>
+        ), node, function () {
+            assert.equals(node.textContent, 'wrapper resources');
+        });
+    });
+
+    test('that the users route is defined', function () {
+        dom.render((
+            <Router history={createHistory('/users')}>
+                { routes }
+            </Router>
+        ), node, function () {
+            assert.equals(node.textContent, 'wrapper resources');
         });
     });
 });
