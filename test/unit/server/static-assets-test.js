bb636c69de5b447a0c0bd0b5ba83e19b602b416f
Matt Travi <programmer@travi.org>
2015-11-03 00:11:51 -0600

    moved static asset config to separate hapi plugin

---
+++ test/unit/server/static-assets-test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const assets = require('../../../lib/server/static-assets');
+
+suite('static assets', function () {
+    test('that the plugin is defined', function () {
+        assert.equals(assets.register.attributes, {
+            name: 'static-assets'
+        });
+    });
+
+    test('that the static asset routes are configured', function () {
+        const
+            next = sinon.spy(),
+            route = sinon.spy();
+
+        assets.register({route: route}, null, next);
+
+        assert.calledOnce(next);
+        assert.calledWith(route, {
+            method: 'GET',
+            path: '/resources/{param*}',
+            handler: {
+                directory: {
+                    path: 'resources'
+                }
+            }
+        });
+        assert.calledWith(route, {
+            method: 'GET',
+            path: '/favicon.ico',
+            handler: {
+                file: {
+                    path: 'bower_components/travi.org-theme/img/favicon.ico'
+                }
+            }
+        });
+    });
+});
