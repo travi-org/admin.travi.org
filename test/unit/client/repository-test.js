c093ba33652a361bdfcd43cee7e30aa05f7c3878
Matt Travi <programmer@travi.org>
2015-11-21 23:30:48 -0600

    added a repository for fetching data from the server

---
+++ test/unit/client/repository-test.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const
+    any = require('../../helpers/any'),
+    sinon = require('sinon'),
+    proxyquire = require('proxyquire');
+
+suite('client repository', function () {
+    const
+        xhr = sinon.stub(),
+        repository = proxyquire('../../../lib/client/repository', {
+            xhr: xhr
+        });
+
+    test('that resource is requested from server by id', function () {
+        const
+            data = [any.simpleObject()],
+            type = any.string(),
+            id = any.int(),
+            callback = sinon.spy();
+
+        repository.getResource(type, id, callback);
+
+        assert.calledWith(xhr, {url: `/${type}/${id}`});
+
+        xhr.yield(null, {
+            body: JSON.stringify(data)
+        });
+
+        assert.calledWith(callback, null, data);
+    });
+
+    test('that error bubbles when getting a resource by id', function () {
+        const
+            error = any.simpleObject(),
+            callback = sinon.spy(),
+            type = any.string(),
+            id = any.int();
+        xhr.withArgs({url: `/${type}/${id}`}).yields(error);
+
+        repository.getResource(type, id, callback);
+
+        assert.calledWith(callback, error);
+    });
+
+    test('that a list of resources is requested from server', function () {
+        const
+            data = [any.simpleObject()],
+            type = any.string(),
+            callback = sinon.spy();
+
+        repository.getResources(type, callback);
+
+        assert.calledWith(xhr, {url: `/${type}`});
+
+        xhr.yield(null, {
+            body: JSON.stringify(data)
+        });
+
+        assert.calledWith(callback, null, data);
+    });
+
+    test('that error bubbles when getting a resource list', function () {
+        const
+            error = any.simpleObject(),
+            callback = sinon.spy(),
+            type = any.string();
+        xhr.withArgs({url: `/${type}`}).yields(error);
+
+        repository.getResources(type, callback);
+
+        assert.calledWith(callback, error);
+    });
+
+    test('that the list of resource types is requested from server', function () {
+        const
+            data = [any.simpleObject()],
+            callback = sinon.spy();
+
+        repository.getResourceTypes(callback);
+
+        assert.calledWith(xhr, {url: `/`});
+
+        xhr.yield(null, {
+            body: JSON.stringify(data)
+        });
+
+        assert.calledWith(callback, null, data);
+    });
+
+    test('that error bubbles when getting a resource list', function () {
+        const
+            error = any.simpleObject(),
+            callback = sinon.spy();
+        xhr.withArgs({url: `/`}).yields(error);
+
+        repository.getResourceTypes(callback);
+
+        assert.calledWith(callback, error);
+    });
+});
