9a5d0d798786cc8a95b8bf2609a3c36edd2a5d33
Matt Travi <programmer@travi.org>
2016-02-03 17:31:08 -0600

    mapped SET_PRIMARY_NAV action in the reducer

--- test/unit/shared/store/reducer-test.js
+++ test/unit/shared/store/reducer-test.js
@@ -3,17 +3,42 @@
 const
     immutable = require('immutable'),
     reducer = require('../../../../lib/shared/store/reducer'),
+    actions = require('../../../../lib/shared/store/actions'),
     any = require('../../../helpers/any'),
     assert = require('chai').assert;
 
 suite('reducer', () => {
+    let sandbox;
+
+    setup(() => {
+        sandbox = sinon.sandbox.create();
+        sandbox.stub(actions, 'setPrimaryNav');
+    });
+
+    teardown(() => {
+        sandbox.restore();
+    });
+
     test('that state is returned directly without known action', () => {
         const initialState = any.simpleObject();
 
-        assert.equal(reducer(initialState), initialState);
+        assert.equal(reducer(initialState, {}), initialState);
     });
 
     test('that a default initial state is provided', () => {
-        assert.equal(immutable.Map(), reducer());
+        assert.equal(reducer(undefined, {}), immutable.Map());
+    });
+
+    test('that the setPrimaryNav function is called for the SET_PRIMARY_NAV action', () => {
+        const updatedState = any.simpleObject();
+        actions.setPrimaryNav.returns(updatedState);
+
+        assert.equal(
+            reducer(immutable.Map(), {
+                type: 'SET_PRIMARY_NAV',
+                nav: any.simpleObject()
+            }),
+            updatedState
+        );
     });
 });
