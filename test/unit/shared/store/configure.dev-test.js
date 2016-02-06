b1c95bbca8ff00ee1cb041c8d02067185b3f9f69
Matt Travi <programmer@travi.org>
2016-02-05 22:56:35 -0600

    development version of the store factory

---
+++ test/unit/shared/store/configure.dev-test.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const
+    proxyquire = require('proxyquire'),
+    redux = require('redux'),
+    any = require('../../../helpers/any'),
+    reducer = require('../../../../lib/shared/store/reducer');
+
+suite('store creation for development', () => {
+    let sandbox;
+    const
+        enhancer = any.simpleObject(),
+        DevTools = {
+            instrument() {}
+        },
+        configureStore = proxyquire('../../../../lib/shared/store/configure.dev', {
+            '../views/dev/DevTools.jsx': DevTools
+        });
+
+    setup(() => {
+        sandbox = sinon.sandbox.create();
+        sandbox.stub(redux, 'createStore');
+        sandbox.stub(DevTools, 'instrument').returns(enhancer);
+    });
+
+    teardown(() => {
+        sandbox.restore();
+    });
+
+    test('that redux store is created from provided initial state', () => {
+        const
+            initialState = any.simpleObject(),
+            store = any.simpleObject();
+        redux.createStore.withArgs(reducer, initialState, enhancer).returns(store);
+
+        assert.equals(configureStore(initialState), store);
+    });
+});
