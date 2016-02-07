1cb6a0a1d84363c4db41e273c121a54bf8604e55
Matt Travi <programmer@travi.org>
2016-02-06 22:01:15 -0600

    configured webpack to use uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,32 @@
 'use strict';
 
-const path = require('path');
+const
+    path = require('path'),
+    webpack = require('webpack');
 
 module.exports = {
     entry: ['./lib/client/app.jsx'],
     module: {
         loaders: [{
             test: /\.jsx?$/,
+            include: /lib\/(client|shared)/,
             loader: 'babel'
         }]
     },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                "NODE_ENV": JSON.stringify("production")
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin()
+    ],
     output: {
         path: path.join(__dirname, 'resources/js'),
         filename: 'bundle.js'
