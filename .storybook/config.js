f9e7e0fc48d6f0cda218404f3a6e312ff2332f9f
Matt Travi <programmer@travi.org>
2016-04-14 00:10:21 -0500

    added stories for the resource list

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,7 @@ import { configure } from '@kadira/storybook';
 
 function loadStories() {
     require('../lib/shared/views/stories/primary-nav.jsx');
+    require('../lib/shared/views/stories/resource-list.jsx');
 }
 
 configure(loadStories, module);
---
+++ lib/shared/views/stories/resource-list.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {storiesOf} from '@kadira/storybook';
+import createResourceList from '../resources/list/maybe-list.jsx';
+
+const ResourceList = createResourceList(React);
+
+require('./theme-for-webpack.scss');
+
+storiesOf('Resource List', module)
+    .add('empty list', () => <ResourceList resourceType="foo" resources={[]}/>)
+    .add('rides list', () => <ResourceList resourceType="rides" resources={[
+        { id: 1, displayName: 'corvette', links: [] },
+        { id: 2, displayName: 'truck', links: [] },
+        { id: 3, displayName: 'camaro', links: [] }
+    ]}/>)
+    .add('users list', () => <ResourceList resourceType="users" resources={[
+        {
+            id: 1,
+            displayName: 'Matt Travi',
+            thumbnail: {src: 'https://www.gravatar.com/avatar/f69785efc7d990da20f1ab49fc2a6648?size=32'},
+            links: []
+        }
+    ]}/>);
