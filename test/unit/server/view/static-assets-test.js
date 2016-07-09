ad5404e0069046c2f5b3c83a3829bccef0392051
Matt Travi <programmer@travi.org>
2016-07-08 23:23:17 -0500

    proxied assets to the webpack server when in dev mode for #305

--- test/unit/server/view/static-assets-test.js
+++ test/unit/server/view/static-assets-test.js
@@ -3,6 +3,10 @@ import {assert} from 'chai';
 import sinon from 'sinon';
 
 suite('static assets', () => {
+    teardown(() => {
+        process.env.NODE_PATH = null;
+    });
+
     test('that the plugin is defined', () => {
         assert.deepEqual(assets.register.attributes, {
             name: 'static-assets'
@@ -32,6 +36,27 @@ suite('static assets', () => {
             handler: {
                 file: {
                     path: 'node_modules/@travi/travi.org-theme/img/favicon.ico'
+                }
+            }
+        });
+    });
+
+    test('that requests for assets are proxied in development', () => {
+        const
+            next = sinon.spy(),
+            route = sinon.spy();
+        process.env.NODE_ENV = 'development';
+
+        assets.register({route}, null, next);
+
+        assert.calledWith(route, {
+            method: 'GET',
+            path: '/resources/{param*}',
+            handler: {
+                proxy: {
+                    host: '0.0.0.0',
+                    port: 3000,
+                    passThrough: true
                 }
             }
         });
