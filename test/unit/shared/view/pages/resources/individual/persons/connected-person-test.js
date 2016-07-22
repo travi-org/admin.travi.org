49acee3ebe2253689dbf7d27d5d1781fe0db0253
Matt Travi <programmer@travi.org>
2016-07-21 23:35:21 -0500

    first step to start using redial

---
+++ test/unit/shared/view/pages/resources/individual/persons/connected-person-test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {createStore} from 'redux';
+import {fromJS} from 'immutable';
+import connectedPerson from '../../../../../../../../lib/shared/views/resources/individual/persons/connected-person';
+
+import {assert} from 'chai';
+import {shallow} from 'enzyme';
+import {string, url, simpleObject} from '@travi/any';
+
+suite('connected person component', () => {
+    const ConnectedPerson = connectedPerson(React);
+
+    test('that redux state is mapped to props', () => {
+        const
+            resource = {
+                id: string(),
+                displayName: string(),
+                name: {
+                    first: string(),
+                    last: string()
+                },
+                links: simpleObject(),
+                thumbnail: url()
+            },
+
+            wrapper = shallow(<ConnectedPerson store={createStore(() => fromJS({resource}))}/>),
+            person = wrapper.prop('person');
+
+        assert.equal(person.id, resource.id);
+        assert.equal(person.displayName, resource.displayName);
+        assert.deepEqual(person.name, resource.name);
+        assert.deepEqual(person.links, resource.links);
+        assert.equal(person.avatar, resource.thumbnail);
+    });
+
+    test('that a `fetch` hook is defined', () => {
+        assert.isFunction(ConnectedPerson['@@redial-hooks'].fetch);
+    });
+});
