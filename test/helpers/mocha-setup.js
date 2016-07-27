9e82f83704b09fde0cfdbdcdb594c4058b9ebaa2
Matt Travi <programmer@travi.org>
2016-07-27 01:03:47 -0500

    added person loader

--- test/helpers/mocha-setup.js
+++ test/helpers/mocha-setup.js
@@ -2,6 +2,7 @@ import jsdom from 'jsdom';
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
 import chaiAsPromised from 'chai-as-promised';
+import chaiSubset from 'chai-subset';
 import sinon from 'sinon';
 import referee from 'referee';
 import 'sinon-as-promised';
@@ -22,6 +23,7 @@ require.extensions['.scss'] = () => undefined;
 
 chai.use(chaiImmutable);
 chai.use(chaiAsPromised);
+chai.use(chaiSubset);
 sinon.assert.expose(chai.assert, { prefix: '' });
 
 referee.format = require('formatio').configure({quoteStrings: false}).ascii;
--- test/unit/shared/view/persons/individual/duck-test.js
+++ test/unit/shared/view/persons/individual/duck-test.js
@@ -1,7 +1,13 @@
 import {Map, fromJS} from 'immutable';
-import reducer from '../../../../../../lib/shared/views/resources/persons/individual/duck';
+import reducer, {
+    loadPerson,
+    LOAD_PERSON,
+    PERSON_LOAD_FAILED,
+    PERSON_LOADED
+} from '../../../../../../lib/shared/views/resources/persons/individual/duck';
 import {assert} from 'chai';
 import any from '@travi/any';
+import sinon from 'sinon';
 
 suite('person duck', () => {
     suite('reducer', () => {
@@ -16,12 +22,12 @@ suite('person duck', () => {
         });
 
         test('that LOAD_PERSON marks as loading and clears a previous person', () => {
-            assert.equal(reducer(Map(), {type: 'LOAD_PERSON'}), fromJS({loading: true, person: {}}));
+            assert.equal(reducer(Map(), {type: LOAD_PERSON}), fromJS({loading: true, loaded: false, person: {}}));
         });
 
         test('that PERSON_LOADED marks as loaded', () => {
             const resource = any.simpleObject();
-            assert.equal(reducer(Map(), {type: 'PERSON_LOADED', resource}), fromJS({
+            assert.equal(reducer(Map(), {type: PERSON_LOADED, resource}), fromJS({
                 loading: false,
                 loaded: true,
                 person: resource
@@ -30,15 +36,29 @@ suite('person duck', () => {
 
         test('that PERSON_LOAD_FAILED marks as loaded', () => {
             const error = any.simpleObject();
-            assert.equal(reducer(Map(), {type: 'PERSON_LOAD_FAILED', error}), fromJS({
+            assert.equal(reducer(Map(), {type: PERSON_LOAD_FAILED, error}), fromJS({
                 loading: false,
-                loaded: true,
                 error
             }));
         });
     });
 
     suite('action creators', () => {
+        test('that loadPerson defines handlers for fetching data for a person', () => {
+            const
+                id = any.integer(),
+                resource = any.simpleObject(),
+                getResource = sinon.stub(),
+                fetcher = {getResource};
+            getResource.withArgs('persons', id).returns(resource);
 
+            assert.containSubset(loadPerson(id), {
+                initiate: LOAD_PERSON,
+                success: PERSON_LOADED,
+                failure: PERSON_LOAD_FAILED
+            });
+
+            assert.equal(loadPerson(id).fetch(fetcher), resource);
+        });
     });
 });
