aa46839d44a541b448543eec833b6748105f5993
Matt Travi <programmer@travi.org>
2016-07-28 23:30:35 -0500

    hooked the fetch middleware into the store

--- test/unit/shared/store/create-test.js
+++ test/unit/shared/store/create-test.js
@@ -5,6 +5,7 @@ import {simpleObject} from '@travi/any';
 import reducer from '../../../../lib/shared/store/reducer';
 import person from '../../../../lib/shared/views/resources/persons/individual/duck';
 import {configureStore} from '../../../../lib/shared/store/create';
+import fetchMiddleware from '../../../../lib/shared/store/fetch-middleware';
 import * as reduxImmutable from 'redux-immutable';
 import sinon from 'sinon';
 import {assert} from 'chai';
@@ -15,12 +16,16 @@ suite('store creation for production', () => {
     const
         initialState = simpleObject(),
         store = simpleObject(),
+        appliedFetch = simpleObject(),
+        composed = simpleObject(),
         combinedReducer = any.simpleObject();
 
     setup(() => {
         sandbox = sinon.sandbox.create();
         sandbox.stub(reduxImmutable, 'combineReducers').withArgs({legacy: reducer, person}).returns(combinedReducer);
-        sandbox.stub(redux, 'createStore').withArgs(combinedReducer, fromJS(initialState)).returns(store);
+        sandbox.stub(redux, 'createStore').withArgs(combinedReducer, fromJS(initialState), composed).returns(store);
+        sandbox.stub(redux, 'compose');
+        sandbox.stub(redux, 'applyMiddleware').withArgs(fetchMiddleware).returns(appliedFetch);
     });
 
     teardown(() => {
@@ -32,13 +37,16 @@ suite('store creation for production', () => {
     });
 
     test('that redux store is created from provided initial state', () => {
+        redux.compose.withArgs(appliedFetch).returns(composed);
+
         assert.equal(configureStore(initialState), store);
     });
 
     test('that devtools browser extension is initialized if present', () => {
-        const
-            enhancer = simpleObject();
+        const enhancer = simpleObject();
         window.devToolsExtension = sinon.stub().returns(enhancer);
+        redux.compose.withArgs(appliedFetch, enhancer).returns(composed);
+        redux.createStore.withArgs(combinedReducer, fromJS(initialState)).returns(store);
 
         configureStore(initialState);
 
