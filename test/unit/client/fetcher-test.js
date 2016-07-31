629d6984152fad021621c0ca6dac10867053e31c
Matt Travi <programmer@travi.org>
2016-07-31 17:28:24 -0500

    added list to the client side fetcher

--- test/unit/client/fetcher-test.js
+++ test/unit/client/fetcher-test.js
@@ -1,4 +1,4 @@
-import {getResource} from '../../../lib/client/fetcher';
+import {getResource, getResources} from '../../../lib/client/fetcher';
 import sinon from 'sinon';
 import xhr from 'xhr';
 import {assert} from 'chai';
@@ -16,25 +16,50 @@ suite('client-side data fetcher', () => {
         sandbox.restore();
     });
 
-    test('that a GET request is made for an individual resource', () => {
-        const
-            type = any.string(),
-            id = any.integer(),
-            response = any.simpleObject();
+    suite('individual resource', () => {
+        test('that a GET request is made for an individual resource', () => {
+            const
+                type = any.string(),
+                id = any.integer(),
+                response = any.simpleObject();
 
-        xhr.get.withArgs(`/${type}/${id}`).yields(null, response);
+            xhr.get.withArgs(`/${type}/${id}`).yields(null, response);
 
-        return assert.becomes(getResource({type, id}), response);
+            return assert.becomes(getResource(type, id), response);
+        });
+
+        test('that a fetch error results in a rejected promise', () => {
+            const
+                type = any.string(),
+                id = any.integer(),
+                error = any.word();
+
+            xhr.get.yields(error);
+
+            return assert.isRejected(getResource(type, id), new RegExp(error));
+        });
     });
 
-    test('that a fetch error results in a rejected promise', () => {
-        const
-            type = any.string(),
-            id = any.integer(),
-            error = any.word();
+    suite('resource list', () => {
+        test('that a GET request is made for an individual resource', () => {
+            const
+                type = any.string(),
+                response = any.simpleObject();
 
-        xhr.get.yields(error);
+            xhr.get.withArgs(`/${type}`).yields(null, response);
 
-        return assert.isRejected(getResource({type, id}), new RegExp(error));
+            return assert.becomes(getResources(type), response);
+        });
+
+        test('that a fetch error results in a rejected promise', () => {
+            const
+                type = any.string(),
+                error = any.word();
+
+            xhr.get.yields(error);
+
+            return assert.isRejected(getResources(type), new RegExp(error));
+        });
+
     });
 });
