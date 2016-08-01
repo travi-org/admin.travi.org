3cd3ccdc61cc5a91a57d851c306bba51141f0f91
Matt Travi <programmer@travi.org>
2016-08-01 00:19:48 -0500

    cleaned up after fetching list data with redial

--- test/unit/client/repository-test.js
+++ test/unit/client/repository-test.js
@@ -1,4 +1,4 @@
-import {simpleObject, string} from '@travi/any';
+import {simpleObject} from '@travi/any';
 import {assert} from 'chai';
 import sinon from 'sinon';
 import proxyquire from 'proxyquire';
@@ -7,35 +7,6 @@ suite('client repository', () => {
     const
         xhr = sinon.stub(),
         repository = proxyquire('../../../lib/client/repository', {xhr});
-
-    test('that a list of resources is requested from server', () => {
-        const
-            data = [simpleObject()],
-            type = string(),
-            callback = sinon.spy();
-
-        repository.getResources(type, callback);
-
-        assert.calledWith(xhr, {url: `/${type}`});
-
-        xhr.yield(null, {
-            body: JSON.stringify(data)
-        });
-
-        assert.calledWith(callback, null, data);
-    });
-
-    test('that error bubbles when getting a resource list', () => {
-        const
-            error = simpleObject(),
-            callback = sinon.spy(),
-            type = string();
-        xhr.withArgs({url: `/${type}`}).yields(error);
-
-        repository.getResources(type, callback);
-
-        assert.calledWith(callback, error);
-    });
 
     test('that the list of resource types is requested from server', () => {
         const
