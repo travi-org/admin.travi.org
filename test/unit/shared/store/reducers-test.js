9da8f90f7d9db336c5b39e3a217372db4d17abdf
Matt Travi <programmer@travi.org>
2016-08-09 22:49:54 -0500

    included the site-level reducer

--- test/unit/shared/store/reducers-test.js
+++ test/unit/shared/store/reducers-test.js
@@ -4,6 +4,7 @@ import legacy from '../../../../lib/shared/store/reducer';
 import person from '../../../../lib/shared/views/persons/individual/duck';
 import resource from '../../../../lib/shared/views/resources/individual/duck';
 import resources from '../../../../lib/shared/views/resources/list/duck';
+import wrap from '../../../../lib/shared/views/theme/wrap/duck';
 
 import sinon from 'sinon';
 import {assert} from 'chai';
@@ -23,7 +24,7 @@ suite('reducers', () => {
 
     test('that the reducers are combined', () => {
         const reducers = any.simpleObject();
-        redux.combineReducers.withArgs({legacy, person, resource, resources}).returns(reducers);
+        redux.combineReducers.withArgs({legacy, person, resource, resources, wrap}).returns(reducers);
 
         assert.equal(getCombined(), reducers);
     });
