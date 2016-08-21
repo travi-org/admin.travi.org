04d1720236f61ee3773616512deeff80f79548ab
Matt Travi <programmer@travi.org>
2016-08-20 23:01:51 -0500

    lint fix

--- test/unit/shared/view/errors/page-test.js
+++ test/unit/shared/view/errors/page-test.js
@@ -2,9 +2,6 @@ import React from 'react';
 import {assert} from 'chai';
 import {shallow} from 'enzyme';
 import ErrorPage from '../../../../../lib/shared/views/errors/page';
-import {createServerError} from '@travi/admin.travi.org-components';
-
-const ServerError = createServerError(React);
 
 suite('error page', () => {
     test('that the error component is wrapped into a page', () => {
