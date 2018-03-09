abb47f3ef4f89cbd516de2f13dbfb2a24109f5f7
Matt Travi <programmer@travi.org>
2018-03-08 22:00:34 -0600

    ci(pact): published the pact to the broker
    for #20

--- scripts/publish-pacts-to-broker.js
+++ scripts/publish-pacts-to-broker.js
@@ -4,10 +4,9 @@ import {resolve} from 'path';
 require('dotenv').config({silent: true});
 
 pact.publishPacts({
-  pactUrls: [resolve(__dirname, '../pacts/travi.org-admin-travi-api.json')],
+  pactFilesOrDirs: [resolve(__dirname, '../pacts/travi.org-admin-travi-api.json')],
   pactBroker: 'https://pact-api.travi.org/',
   pactBrokerUsername: process.env.PACT_BROKER_USER,
   pactBrokerPassword: process.env.PACT_BROKER_PASSWORD,
-  consumerVersion: '0.0.3'
-  //  tags: <Array>,                  // An array of Strings to tag the Pacts being published. Optional
+  consumerVersion: process.env.TRAVIS_COMMIT
 }).catch(e => console.error(e));      // eslint-disable-line no-console
